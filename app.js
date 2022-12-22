d2480547a25ed8e6156cb329de67993bf15f63b0
Ömer Güneş <omergunes2700@gmail.com>
2022-12-23 02:14:09 +0300

    Localdeki Json dosyasından fetch ile veri alma

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ function getTextFile() {
     .catch((err) => console.log(err));
 }
 getTextFile();
+
+function getJsonFile() {
+  // Local bir json dosyasından veri alma
+  // Dosyanın adresini yazıyoruz
+  fetch("example.json")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+getJsonFile();
